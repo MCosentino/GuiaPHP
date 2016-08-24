b807f37c9b21049cf371ba76c382e442d71cc8c5
MCosentino <muc_358@hotmail.com>
2016-08-23 21:59:49 -0300

    inicio con objetos

---
+++ FiguraGeometrica.php
@@ -0,0 +1,24 @@
+<?php
+public class FiguraGeometrica
+{
+	protected $color;
+	protected $perimetro;
+	protected $superficie;
+	
+	function __construct()
+	{
+		$this->color = 'azul';
+		$this->perimetro = 14;
+		$this->superficie = 10;
+	}
+
+	function getColor()
+	{
+
+	}
+
+	function setColor()
+	{
+		
+	}
+} ?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	include "FiguraGeometrica.php";	
+	include_once "FiguraGeometrica.php";
+	
+	$figura = new FiguraGeometrica();
+	var_dump($figura);
+	?>
+</body>
+</html>
